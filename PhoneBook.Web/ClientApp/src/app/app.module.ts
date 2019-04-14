70a92dd74943acdd27e0fce1fa2820e15060f30e
Mladen <mladsdf@sdf.com>
2019-04-14 21:03:05 +0200

    removed unneeded stuff

--- PhoneBook.Web/ClientApp/src/app/app.module.ts
+++ PhoneBook.Web/ClientApp/src/app/app.module.ts
@@ -6,9 +6,6 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ContactListComponent } from './contacts-list/contact-list.component';
 import { ContactDetailsComponent } from './contact-details/contact-details.component';
 import { ContactDetailsEditor as ContactDetailsEditorComponent } from './contact-details-editor/contact-details-editor.component';
@@ -18,9 +15,6 @@ import { ContactDetailsReadonly } from './contact-details-readonly/contact-detai
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     ContactListComponent,
     ContactDetailsComponent,
     ContactDetailsEditorComponent,
@@ -35,10 +29,6 @@ import { ContactDetailsReadonly } from './contact-details-readonly/contact-detai
       { path: '', component: ContactListComponent, pathMatch: 'full' },
       { path: 'contacts', component: ContactListComponent },
       { path: 'contact/:id', component: ContactDetailsComponent },
-      
-      { path: 'home', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
   providers: [],
